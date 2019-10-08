bfc9dac4596942dc9655d58d14c54edb2b319e7e
tuffsruffs <tuffsruffs@gmail.com>
2019-10-08 10:01:13 +0200

    Add uppdate

---
+++ my_bookinga_app/src/components/Login.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Header.css';
+
+class Login extends React.Component {
+    render() {
+        return (
+            <div id="Login">
+                <span>Login</span>
+            </div>
+        );
+    }
+}
+
+export default Login;
